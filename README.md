50e1888f09c4f0fc91c52a70abb1ab78f203d2bb
Luke Miles <luke@lukemil.es>
2015-01-04 21:59:02 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+api.lukemil.es
+===============
+
+the beginnings of my personal status API. still a work in progress.
+
+found at [api.lukemil.es](https://api.lukemil.es).
---
+++ start-on-boot.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# This script launches the site on boot
+
+# Change to node directory
+cd /home/luke/api.lukemil.es/
+# Start nodemon on 3000 (iptables corrects route to port 80)
+NODE_ENV=production PORT=3000 nodemon
