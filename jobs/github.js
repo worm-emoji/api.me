22a0a99a318c1ea9f770ea8303dc7d83faff7dd7
Luke Miles <luke@lukemil.es>
2015-01-08 11:02:27 -0800

    add github recent commits job

--- jobs/github.js
+++ jobs/github.js
@@ -1,16 +1,17 @@
 // Get github contributions every 5 min
 // We don't use the GitHub api here because there's no contributions API
 
+//request for simple http calls
+var request = require('request');
+//save.js to save json
+var save = require('../save.js');
+//get username from config file
+var config = require('../config.js');
+
 job('contributions', function(done) {
 
-	//request for simple http calls
-	var request = require('request');
 	//jQuery to scrape DOM
 	$ = require('jquery');
-	//save.js to save json
-	var save = require('../save.js');
-	//get username from config file
-	var config = require('../config.js');
 
 	// set up array
 	jsonData = new Object;
@@ -26,9 +27,61 @@ job('contributions', function(done) {
 	 	contributions = parseInt(contributions);
 	 	//add to array
 	 	jsonData.contributions = contributions;
-	 	save.file('contributions', jsonData);
+	 	save.file('github-contributions', jsonData);
 	 	console.log('GitHub contributions updated.');
+	 	done(jsonData);
 	  }
 	});
 
-}).every('12h');
+}).every('10m');
+
+job('lastCommit', function(done, contributions) {
+
+	//http options
+	var options = {
+	    url: 'https://api.github.com/users/' + config.github.username +'/events',
+	    headers: {
+	        'User-Agent': 'api.lukemil.es',
+	        'Accept': 'Accept: application/vnd.github.v3+json'
+	    }
+	};
+
+	request(options, function (error, response, data) {
+	  if (!error && response.statusCode == 200) {
+	  	// deserialize json
+	  	data = JSON.parse(data);
+	  	var keys = Object.keys(data);
+	  	// iterate through keys
+	  	 for(var i = 0, length = keys.length; i < length; i++) {
+	  	 	//create object for current event
+	  	 	currentEvent = data[keys[i]];
+	  	 	if (currentEvent.type === "PushEvent") {
+	  	 		lastPush = new Object;
+	  	 		lastPush.repo_url = "https://github.com/" + currentEvent.repo.name;
+	  	 		lastPush.commit_url = lastPush.repo_url + "/commit/" + currentEvent.payload.head;
+	  	 		lastPush.commit_message = currentEvent.payload.commits[0].message;
+	  	 		lastPush.time = currentEvent.created_at;
+	  	 		lastPush.repo_name =  currentEvent.repo.name;
+	
+
+	  	 		//create github json file
+
+	  	 		github = new Object;
+	  	 		github.lastPush = lastPush;
+	  	 		github.contributions = contributions.contributions;
+
+	  	 		// save everything
+
+	  	 		save.file('github-last', lastPush);
+	  	 		console.log('Last github commit updated.');
+	  	 		save.file('github', github);
+	  	 		console.log('github.json updated');
+
+
+	  	 		break;
+	  	 	}
+	  }
+	}
+
+	});
+}).after('contributions')
