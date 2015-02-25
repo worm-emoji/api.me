1ee2273bc71e91f09c555683424a01a5b3d74be6
Luke Miles <luke@lukemil.es>
2015-02-24 18:32:53 -0800

    Change to forever to recover from crashes automatically

--- start-on-boot.sh
+++ start-on-boot.sh
@@ -4,4 +4,4 @@
 # Change to node directory
 cd /home/luke/api.lukemil.es/
 # Start nodemon on 3000 (iptables corrects route to port 80)
-NODE_ENV=production PORT=3000 nodemon
+NODE_ENV=production PORT=3000 forever bin/www -c
